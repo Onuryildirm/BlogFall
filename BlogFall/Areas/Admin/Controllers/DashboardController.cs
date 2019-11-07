2de6654492c839701015e841cbf4ca4eb91295c3
onur.yildirim <onur.yildirm@gmail.com>
2019-11-07 21:32:51 +0300

    Profil fotoğrafı eklendi

--- BlogFall/Areas/Admin/Controllers/DashboardController.cs
+++ BlogFall/Areas/Admin/Controllers/DashboardController.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
 namespace BlogFall.Areas.Admin.Controllers
 {
-    public class DashboardController : Controller
+    //Authorize(Roles = "Admin") miras olarak geliyor
+    public class DashboardController : AdminBaseController
     {
         // GET: Admin/Dashboard
         public ActionResult Index()
